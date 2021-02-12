077d364701076a02ca1c7ed35f9982fe6d61c16e
imrazave <imrazave@gmail.com>
2021-02-11 19:53:50 -0600

    first commit

---
+++ TvDinner.Data/recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvDinner.Data
+{
+    public class recipe
+    {
+        [Key]
+        public int RecipeId { get; set; }
+        [Required]
+        public string RecipeName { get; set; }
+        [Required]
+        public List<string> RecipeIngredients { get; set; }
+        [Required]
+        public List<string> Instructions { get; set; }
+        public int Servings { get; set; }
+        public int CaloriesPerServing { get; set; }
+    }
+}
