9dca4ed04628a5b5925d4df2692d427294ac8741
Erika <erikapeltz0@gmail.com>
2021-02-11 21:15:35 -0500

    classes

---
+++ TvDinner.Data/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvDinner.Data
+{
+    public class Location
+    {
+        [Key]
+        public int LocationID { get; set; }
+        public string Continent { get; set; }
+        public string Country { get; set;}
+        public string State_Territory { get; set; }
+        public string City { get; set; }
+    } 
+}
