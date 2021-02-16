beb71bc0ee5138f7ad6d86ed20118752d59ba2d9
Erika <erikapeltz0@gmail.com>
2021-02-16 18:42:43 -0500

    update

--- TvDinner.Services/LocationServices.cs
+++ TvDinner.Services/LocationServices.cs
@@ -37,14 +37,14 @@ namespace TvDinner.Services
 
             }
 
-            public IEnumerable<LocationList> GetLocation()
+            public IEnumerable<LocationList> GetLocation(int id)
             {
                 using (var ctx = new ApplicationDbContext())
                 {
                     var query =
                         ctx
                             .Locations
-
+                            .Where(e => e.LocationID == id)
                             .Select(
                                 e =>
                                     new LocationList
