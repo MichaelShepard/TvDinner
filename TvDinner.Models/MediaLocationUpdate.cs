5b6213ff60670924344b418d4302fea8ff75876e
MichaelShepard <mike@rockcastlepartners.com>
2021-03-02 17:43:42 -0500

    added location search

---
+++ TvDinner.Models/MediaLocationUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvDinner.Models
+{
+    public class MediaLocationUpdate
+    {
+        public int MediaId { get; set; }
+
+        public int LocationID { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+
+    }
+}
