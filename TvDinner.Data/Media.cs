9db4ca99374b8f35caa48b5e564125c4d3c345a4
MichaelShepard <mike@rockcastlepartners.com>
2021-03-04 17:55:55 -0500

    removed location FK thing

--- TvDinner.Data/Media.cs
+++ TvDinner.Data/Media.cs
@@ -63,7 +63,7 @@ namespace TvDinner.Data
 
 
         public virtual ICollection<Recipe> Recipes { get; set; }
-        // public virtual ICollection<Location> Locations { get; set; }
+        
 
     }
 }
