1f43cb4a816c034ed315ed731d14561a7b5168ae
MichaelShepard <mike@rockcastlepartners.com>
2021-03-07 18:11:50 -0500

    added LocationID to the LocationEdit model

--- TvDinner.Models/LocationEdit.cs
+++ TvDinner.Models/LocationEdit.cs
@@ -9,15 +9,21 @@ namespace TvDinner.Models
 {
     public class LocationEdit
     {
+
+        public int LocationID { get; set; }
+
         [Required]
         [MaxLength(500, ErrorMessage = "500 or less characters please.")]
         public string Continent { get; set; }
+        
         [Required]
         [MaxLength(500, ErrorMessage = "500 or less characters please.")]
         public string Country { get; set; }
+        
         [Required]
         [MaxLength(500, ErrorMessage = "500 or less characters please.")]
         public string State_Territory { get; set; }
+       
         [Required]
         [MaxLength(500, ErrorMessage = "500 or less characters please.")]
         public string City { get; set; }
